26bab3404e226d537970d5c27973b787484a5fe6
toanhhg123 <toanhhg123@gmail.com>
2023-10-28 11:29:22 +0700

    add cors and express middleware

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,14 @@ import serverApollo from './apollo'
 const server = async () => {
   const app = express()
 
+  app.use(cors())
+  app.use(express.json())
+  app.use(
+    express.urlencoded({
+      extended: true
+    })
+  )
+
   await serverApollo.start()
 
   app.use(
